527247d8b6c709b5c56a13c4c66b5a15827f4e82
Pastor-git <maciej.pastor@gmail.com>
2023-06-24 19:03:11 +0200

    Main clean

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,23 +8,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String label = "label";
-        System.out.println("Hello world!");
-//        ZMIENNA STRING NAZWA GARCZA KTÓRY WYGRAŁ
-//        OKNO POWITALNE:
-//        1: MAIN FRAME Z ARGUMENTEM STARTGAME
-//        2: MAIN FRAME Z ARGUMENTEM LOADGAME
-//        nuem NAZYW PRZYCISKÓW MENU przekazuje nazwę przyciusku do
-//        Main Frame => Main Panel który wybiera metodę budowy tablicy int => LOADGAME lub SAVEGMAE
+        String label = "basicState";
 
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 WelcomeMenu welcomeMenu = new WelcomeMenu(label);
-//                MainFrame mainFrame = new MainFrame(label);
-//                1. tworzymy też MAIN fraim którą wstrzykujemy do WELCOM MENNU
-//                2. MAIN FRAME ma metody przekazywane do listienra w welcom screen inicjalizujace MAIN PANEL z grą
-
             }
         }
         );
